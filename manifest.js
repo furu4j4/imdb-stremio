a4230b8601deb8d00e9e39085b52ffa3e18a101b
furu4j4 <samiu141414@gmail.com>
2026-04-15 16:54:06 +0100

    Modify manifest ID, version, and behavior hints
    Updated the manifest ID and version, and added behavior hints.

--- manifest.js
+++ manifest.js
@@ -1,11 +1,15 @@
 module.exports = {
-    id: "org.imdb.parentsguide",
-    version: "1.0.0",
+    id: "org.imdb.parentsguide.final",
+    version: "1.0.2",
     name: "IMDb Parents Guide",
     description: "Displays IMDb Parental Advisory (Nudity, Violence, etc.)",
     resources: ["stream"],
     types: ["movie", "series"],
     idPrefixes: ["tt"],
     catalogs: [],
-    logo: "https://www.imdb.com/favicon.ico"
+    logo: "https://www.imdb.com/favicon.ico",
+    behaviorHints: {
+        configurable: false,
+        configurationRequired: false
+    }
 };
