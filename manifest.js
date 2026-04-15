2392d66618feda38ffcfd100175e1b321d5b8671
furu4j4 <samiu141414@gmail.com>
2026-04-15 18:04:39 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,15 +1,19 @@
 module.exports = {
-    id: "org.imdb.parentsguide.final",
-    version: "1.0.2",
-    name: "IMDb Parents Guide",
-    description: "Displays IMDb Parental Advisory (Nudity, Violence, etc.)",
+    id: "org.imdb.parentsguide.scraperapi",
+    version: "1.1.0",
+    name: "IMDb Parents Guide (ScraperAPI)",
+    description: "IMDb Parental Advisory via ScraperAPI",
     resources: ["stream"],
     types: ["movie", "series"],
     idPrefixes: ["tt"],
     catalogs: [],
-    logo: "https://www.imdb.com/favicon.ico",
-    behaviorHints: {
-        configurable: false,
-        configurationRequired: false
-    }
+    // This tells Stremio to look for a key in the URL
+    config: [
+        {
+            key: "apiKey",
+            type: "text",
+            title: "ScraperAPI Key",
+            required: true
+        }
+    ]
 };
