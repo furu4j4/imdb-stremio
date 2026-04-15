2bc8d56e122b824e1379fda51ce85e852c281bea
furu4j4 <samiu141414@gmail.com>
2026-04-15 18:50:15 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,14 +1,21 @@
 module.exports = {
     id: "org.imdb.parentsguide.scraperapi",
-    version: "1.2.0",
+    version: "1.1.2",
     name: "IMDb Parents Guide",
     description: "IMDb Parental Advisory via ScraperAPI",
     resources: ["stream"],
     types: ["movie", "series"],
     idPrefixes: ["tt"],
     catalogs: [],
-    behaviorHints: {
-        configurable: true,
-        configurationRequired: true
-    }
+    logo: "https://www.imdb.com/favicon.ico",
+    // This defines the setup page
+    config: [
+        {
+            key: "apiKey",
+            type: "text",
+            title: "ScraperAPI Key",
+            placeholder: "Enter your ScraperAPI key...",
+            required: true
+        }
+    ]
 };
