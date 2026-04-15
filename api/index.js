794e06f02deadd1e49df5c37fa8e7468c377c565
furu4j4 <samiu141414@gmail.com>
2026-04-15 18:04:06 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,34 @@ const path = require('path');
 const addonInterface = require(path.join(__dirname, '..', 'addon.js'));
 
 const app = express();
-const router = getRouter(addonInterface);
 
-app.use('/', router);
+// 1. The Configuration Page (HTML)
+app.get('/', (req, res) => {
+    res.send(`
+        <div style="text-align: center; font-family: sans-serif; padding-top: 50px;">
+            <h1>IMDb Parents Guide Configuration</h1>
+            <p>Enter your ScraperAPI Key to generate your install link:</p>
+            <input type="text" id="key" placeholder="Paste ScraperAPI Key here" style="padding: 10px; width: 300px;">
+            <br><br>
+            <button onclick="install()">Generate Install Link</button>
+            <script>
+                function install() {
+                    const key = document.getElementById('key').value;
+                    if(!key) return alert('Please enter a key');
+                    const url = window.location.host + '/' + key + '/manifest.json';
+                    window.location.href = 'stremio://' + url;
+                }
+            </script>
+        </div>
+    `);
+});
+
+// 2. Routing logic to catch the API key from the URL
+app.use('/:apiKey', (req, res, next) => {
+    const router = getRouter(addonInterface);
+    // We attach the apiKey to the request so addon.js can see it
+    req.apiKey = req.params.apiKey; 
+    router(req, res, next);
+});
 
 module.exports = app;
