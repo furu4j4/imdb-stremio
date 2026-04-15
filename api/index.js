977b72eeef985bc89823e96d6cf6cc90b3c37e5b
furu4j4 <samiu141414@gmail.com>
2026-04-15 18:31:15 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const { getRouter } = require('stremio-addon-sdk');
-const cors = require('cors');
 const path = require('path');
+const cors = require('cors');
 const addonInterface = require('../addon.js');
 
 const app = express();
-
-// Use standard CORS package for better compatibility
-app.use(cors());
+app.use(cors()); // Helps Stremio talk to your server
 
 const router = getRouter(addonInterface);
 
-// This serves the manifest and the configuration landing page
+// Use the SDK router for all paths
 app.use('/', router);
+
+// If someone hits the root "/", send them to the config page instead of a 404
+app.get('/', (req, res) => {
+    res.redirect('/configure');
+});
 
 module.exports = app;
