229754a2129f28c03eb5fcfa776ba2629ff5baf0
furu4j4 <samiu141414@gmail.com>
2026-04-15 18:14:47 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { getRouter } = require('stremio-addon-sdk');
 const path = require('path');
+const cors = require('cors');
 const addonInterface = require(path.join(__dirname, '..', 'addon.js'));
 
 const app = express();
-const router = getRouter(addonInterface);
 
-// This allows the add-on to work with both /manifest.json and /config=.../manifest.json
+// Enable CORS for Stremio Web and other clients
+app.use(cors());
+
+const router = getRouter(addonInterface);
 app.use('/', router);
 
 module.exports = app;
